fe8a6160c6c1a07f23c149f8a7e6914e2fd3ae94
Dmitry Dudin <dudin.tv@gmail.com>
2024-02-29 20:02:21 +0100

    feat: introduce common layout component

---
+++ src/layouts/TheLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="absolute top-0 w-full h-[30%] opacity-20 radial-gradient"></div>
+  <slot />
+</template>
+
+<style lang="scss" scoped>
+.radial-gradient {
+  background: radial-gradient(
+    50% 150% at 50% -50%,
+    theme('colors.primary') 0%,
+    theme('colors.primary/0') 100%
+  );
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./src/**/*.{vue,ts}'],
   theme: {
+    colors: {
+      primary: '#3D948B',
+      secondary: '#943D8C',
+      ternary: '#50482B',
+      accent: '#F5C518',
+      mutedWhite: '#F5C518',
+      background: '#20302E',
+    },
     extend: {},
   },
   plugins: [],
