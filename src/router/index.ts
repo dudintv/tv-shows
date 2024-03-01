52f215735dbd93ac302bf05e5ac7edc51b06ca43
Dmitry Dudin <dudin.tv@gmail.com>
2024-03-01 17:00:40 +0100

    feat: introduce routes: dashboard, search result, and show details

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: () => import('@/views/DashboardPage.vue') },
+    { path: '/shows/:id', component: () => import('@/views/ShowPage.vue') },
+    { path: '/search', component: () => import('@/views/SearchPage.vue') },
+  ],
 });
 
 export default router;
---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>DASHBOARD</h1>
+</template>
+
+<style lang="scss" scoped></style>
