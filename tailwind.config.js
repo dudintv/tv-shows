1714337c7b2db401b0dc8243c05a99ef92304230
Dmitry Dudin <dudin.tv@gmail.com>
2024-03-01 09:58:35 +0100

    fix: extend tailwind colors instead of replacement

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,19 @@
 export default {
   content: ['./src/**/*.{vue,ts}'],
   theme: {
-    colors: {
-      primary: '#3D948B',
-      secondary: '#943D8C',
-      ternary: '#50482B',
-      accent: '#F5C518',
-      mutedWhite: '#F5C518',
-      background: '#20302E',
+    extend: {
+      colors: {
+        primary: '#3D948B',
+        secondary: '#943D8C',
+        ternary: '#50482B',
+        accent: '#F5C518',
+        'muted-white': '#FFFBEB',
+        background: '#20302E',
+      },
+      screens: {
+        xs: '420px',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
